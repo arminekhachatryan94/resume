5a2b0832651ff21d23a6ee9cfd033ebace248455
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-05-23 22:08:48 -0700

    added scroll bar & styling/functionality to navbar

--- script.js
+++ script.js
@@ -1,122 +1,158 @@
 
 $(document).ready(function(){
     $('#skills').click( function(){
-        $(this).css('background-color', 'blue');
+        $(this).css('background-color', 'lightblue');
+        $(this).css('color', 'darkblue');
         $('.skills').css('display', 'block');
 
         $('#projects').css('background-color', 'transparent');
+        $('#projects').css('color', 'white');
         $('.projects').css('display', 'none');
 
         $('#education').css('background-color', 'transparent');
+        $('#education').css('color', 'white');
         $('.education').css('display', 'none');
         
         $('#experience').css('background-color', 'transparent');
+        $('#experience').css('color', 'white');
         $('.experience').css('display', 'none');
 
         $('#extracurricular').css('background-color', 'transparent');
+        $('#extracurricular').css('color', 'white');
         $('.extracurricular').css('display', 'none');
 
         $('#volunteer').css('background-color', 'transparent');
+        $('#volunteer').css('color', 'white');
         $('.volunteer').css('display', 'none');
     });
 
     $('#projects').click( function(){
-        $(this).css('background-color', 'blue');
+        $(this).css('background-color', 'lightblue');
+        $(this).css('color', 'darkblue');
         $('.projects').css('display', 'block');
 
         $('#skills').css('background-color', 'transparent');
+        $('#skills').css('color', 'white');
         $('.skills').css('display', 'none');
 
         $('#education').css('background-color', 'transparent');
+        $('#education').css('color', 'white');
         $('.education').css('display', 'none');
         
         $('#experience').css('background-color', 'transparent');
+        $('#experience').css('color', 'white');
         $('.experience').css('display', 'none');
 
         $('#extracurricular').css('background-color', 'transparent');
+        $('#extracurricular').css('color', 'white');
         $('.extracurricular').css('display', 'none');
 
         $('#volunteer').css('background-color', 'transparent');
+        $('#volunteer').css('color', 'white');
         $('.volunteer').css('display', 'none');
     });
     
     $('#education').click( function(){
-        $(this).css('background-color', 'blue');
+        $(this).css('background-color', 'lightblue');
+        $(this).css('color', 'darkblue');
         $('.education').css('display', 'block');
 
         $('#skills').css('background-color', 'transparent');
+        $('#skills').css('color', 'white');
         $('.skills').css('display', 'none');
 
         $('#projects').css('background-color', 'transparent');
+        $('#projects').css('color', 'white');
         $('.projects').css('display', 'none');
         
         $('#experience').css('background-color', 'transparent');
+        $('#experience').css('color', 'white');
         $('.experience').css('display', 'none');
 
         $('#extracurricular').css('background-color', 'transparent');
+        $('#extracurricular').css('color', 'white');
         $('.extracurricular').css('display', 'none');
 
         $('#volunteer').css('background-color', 'transparent');
+        $('#volunteer').css('color', 'white');
         $('.volunteer').css('display', 'none');
     });
     
     $('#experience').click( function(){
-        $(this).css('background-color', 'blue');
+        $(this).css('background-color', 'lightblue');
+        $(this).css('color', 'darkblue');
         $('.experience').css('display', 'block');
 
         $('#skills').css('background-color', 'transparent');
+        $('#skills').css('color', 'white');
         $('.skills').css('display', 'none');
 
         $('#projects').css('background-color', 'transparent');
+        $('#projects').css('color', 'white');
         $('.projects').css('display', 'none');
 
         $('#education').css('background-color', 'transparent');
+        $('#education').css('color', 'white');
         $('.education').css('display', 'none');
         
         $('#extracurricular').css('background-color', 'transparent');
+        $('#extracurricular').css('color', 'white');
         $('.extracurricular').css('display', 'none');
 
         $('#volunteer').css('background-color', 'transparent');
+        $('#volunteer').css('color', 'white');
         $('.volunteer').css('display', 'none');
     });
     
     $('#extracurricular').click( function(){
-        $(this).css('background-color', 'blue');
+        $(this).css('background-color', 'lightblue');
+        $(this).css('color', 'darkblue');
         $('.extracurricular').css('display', 'block');
 
         $('#skills').css('background-color', 'transparent');
+        $('#skills').css('color', 'white');
         $('.skills').css('display', 'none');
 
         $('#projects').css('background-color', 'transparent');
+        $('#projects').css('color', 'white');
         $('.projects').css('display', 'none');
 
         $('#education').css('background-color', 'transparent');
+        $('#education').css('color', 'white');
         $('.education').css('display', 'none');
         
         $('#experience').css('background-color', 'transparent');
+        $('#experience').css('color', 'white');
         $('.experience').css('display', 'none');
 
         $('#volunteer').css('background-color', 'transparent');
+        $('#volunteer').css('color', 'white');
         $('.volunteer').css('display', 'none');
     });
     
     $('#volunteer').click( function(){
-        $(this).css('background-color', 'blue');
+        $(this).css('background-color', 'lightblue');
+        $(this).css('color', 'darkblue');
         $('.volunteer').css('display', 'block');
 
         $('#skills').css('background-color', 'transparent');
+        $('#skills').css('color', 'white');
         $('.skills').css('display', 'none');
 
         $('#projects').css('background-color', 'transparent');
+        $('#projects').css('color', 'white');
         $('.projects').css('display', 'none');
 
         $('#education').css('background-color', 'transparent');
+        $('#education').css('color', 'white');
         $('.education').css('display', 'none');
         
         $('#experience').css('background-color', 'transparent');
+        $('#experience').css('color', 'white');
         $('.experience').css('display', 'none');
 
         $('#extracurricular').css('background-color', 'transparent');
+        $('#extracurricular').css('color', 'white');
         $('.extracurricular').css('display', 'none');
     });
 });
