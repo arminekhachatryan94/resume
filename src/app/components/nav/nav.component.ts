dde730ce6767d03d69271e61c8e594acacb709a4
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-11-02 19:53:42 -0700

    hamburger bar (items don't drop down yet)

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -22,4 +22,26 @@ export class NavComponent implements OnInit {
   onMouseLeave(name) {
     document.getElementById(name).style.backgroundColor = "";
   }
+
+  hamburgerBar() {
+    var hamburger = document.getElementById("hamburger");
+    var bar1 = document.getElementById("bar1");
+    var bar2 = document.getElementById("bar2");
+    var bar3 = document.getElementById("bar3");
+    if (hamburger.className === "nav text-royalblue navbar-custom float-right") {
+        hamburger.className += " responsive";
+        bar1.style.webkitTransform = "rotate(-45deg) translate(-5px, 3px)";
+        bar1.style.transform = "rotate(-45deg) translate(-5px, 3px)";
+        bar2.style.opacity = "0";
+        bar3.style.webkitTransform = "rotate(45deg) translate(-6px, -6px)";
+        bar3.style.transform = "rotate(45deg) translate(-6px, -6px)";
+    } else {
+        hamburger.className = "nav text-royalblue navbar-custom float-right";
+        bar1.style.webkitTransform = "";
+        bar1.style.transform = "";
+        bar2.style.opacity = "";
+        bar3.style.webkitTransform = "";
+        bar3.style.transform = "";
+    }
+  }
 }
