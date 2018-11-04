8c4222551ef097b213ea80393d48f2307ae52147
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-11-03 23:19:38 -0700

    change color scheme

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -12,7 +12,7 @@ export class NavComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.color = 'rgb(222, 227, 240)';
+    this.color = 'rgb(226, 243, 255)';
   }
 
   onMouseOver(name) {
@@ -28,7 +28,7 @@ export class NavComponent implements OnInit {
     var bar1 = document.getElementById("bar1");
     var bar2 = document.getElementById("bar2");
     var bar3 = document.getElementById("bar3");
-    if (hamburger.className === "nav text-royalblue navbar-custom float-right") {
+    if (hamburger.className === "nav text--blue navbar-custom float-right") {
         hamburger.className += " responsive";
         bar1.style.webkitTransform = "rotate(-45deg) translate(-5px, 3px)";
         bar1.style.transform = "rotate(-45deg) translate(-5px, 3px)";
@@ -36,7 +36,7 @@ export class NavComponent implements OnInit {
         bar3.style.webkitTransform = "rotate(45deg) translate(-6px, -6px)";
         bar3.style.transform = "rotate(45deg) translate(-6px, -6px)";
     } else {
-        hamburger.className = "nav text-royalblue navbar-custom float-right";
+        hamburger.className = "nav text--blue navbar-custom float-right";
         bar1.style.webkitTransform = "";
         bar1.style.transform = "";
         bar2.style.opacity = "";
