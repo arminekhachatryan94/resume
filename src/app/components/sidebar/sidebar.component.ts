c7f67cf31b137427028ecd953fcbb93554937b41
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-11-04 18:41:19 -0800

    pass data from parent to child

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
+  @Input() md_size: boolean;
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
--- src/app/views/portfolio/portfolio.component.ts
+++ src/app/views/portfolio/portfolio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent implements OnInit {
+  md_size: boolean;
 
-  constructor() { }
-
+  constructor() {
+    if( window.innerWidth < 768 ){
+      this.md_size = true;
+    } else {
+      this.md_size = false;
+    }
+  }
+  
   ngOnInit() {
   }
 
+  @HostListener('window:resize') resize() {
+    if( window.innerWidth < 768 ){
+      this.md_size = true;
+    } else {
+      this.md_size = false;
+    }
+  }
 }
