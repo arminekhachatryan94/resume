0e11aad452cf25cfe108d978f15ecdd291a54cb6
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-11-08 22:21:10 -0800

    remove sidebar and change theme

--- src/app/views/portfolio/portfolio.component.ts
+++ src/app/views/portfolio/portfolio.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit, HostListener } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
   md_size: boolean;
+  experiences: Object[];
+  educations: Object[];
   skills: Object;
 
   constructor() {
@@ -28,6 +30,7 @@ export class PortfolioComponent implements OnInit {
   
   ngOnInit() {
     this.setSkills();
+    this.setEducations();
   }
 
   @HostListener('window:resize') resize() {
@@ -60,5 +63,70 @@ export class PortfolioComponent implements OnInit {
       'Agile', 'Scrum', 'Git', 'Github', 'Jira',
       'Unit Testing', 'Automated Testing'
     ]; 
+  }
+
+  setEducations() {
+    this.educations = [
+      {
+        school: 'California State University, Northridge',
+        degree: 'Master of Science',
+        major: 'Software Engineering',
+        start_date: 'Aug 2018',
+        end_date: 'current',
+        gpa: 4.0,
+        honors: "",
+        coursework: [
+          'Software Verification and Validation',
+          'Graphical User Interfaces', 'Software Architecture and Design',
+          'Software Metrics'
+        ]
+      },
+      {
+        school: 'California State University, Northridge',
+        degree: 'Bachelor of Science',
+        major: 'Computer Science',
+        start_date: 'Aug 2016',
+        end_date: 'May 2018',
+        gpa: 3.64,
+        honors: "Dean's list - Fall 2017 and Spring 2018",
+        coursework: [
+          'Algorithms', 'Software Engineering',
+          'Object-Oriented Software Development',
+          '(Advanced) Web Development',
+          'Operating Systems and Computer/System Architecture',
+          'Discrete Mathematics and Probability'
+        ]
+      },
+      {
+        school: 'Los Angeles Pierce College',
+        degree: 'Associate of Science',
+        major: 'Programming for Computer Science, Mathematics for Transfer',
+        start_date: 'Aug 2013',
+        end_date: 'June 2016',
+        gpa: 3.47,
+        honors: "Dean's list - Spring 2015 and Spring 2016",
+        coursework: [
+          'Data Structures and Databases', 'Programming in Java',
+          'Programming in C', 'Object Oriented Programming in C++',
+          'Computer Architecture and Organization', 'Multivariable Calculus',
+          'Linear Algebra', 'Physics'
+        ]
+      }
+    ]
+  }
+
+  setExperiences() {
+    this.experiences = [
+      {
+        start_date: 'Sept 2016',
+        end_date: 'Dec 2017',
+        title: 'Mathematics Tutor'
+      },
+      {
+        start_date: 'Aug 2015',
+        end_date: 'Aug 2016',
+        title: 'Mathematics Tutor'
+      }
+    ]
   }
 }
