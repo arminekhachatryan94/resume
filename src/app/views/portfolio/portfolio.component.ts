ef87b28e394a40db84a7dc348206b9993c06fd73
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-11-10 23:11:13 -0800

    add more data to experience

--- src/app/views/portfolio/portfolio.component.ts
+++ src/app/views/portfolio/portfolio.component.ts
@@ -6,49 +6,14 @@ import { Component, OnInit, HostListener } from '@angular/core';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent implements OnInit {
-  md_size: boolean;
   experience: Object[];
   education: Object[];
   skills: Object;
-
-  constructor() {
-    let width = window.innerWidth;
-    let height = window.innerHeight;
-    
-    if( width/height < .8 ){
-      // landscape: width > height
-      this.md_size = true;
-    }
-    else if( width/height > 1.5 ){
-      // vertical: width < height
-      this.md_size = false;
-    } else {
-      // square: width ~= height
-      this.md_size = false;
-    }
-  }
   
   ngOnInit() {
     this.setExperiences()
     this.setEducations();
     this.setSkills();
-  }
-
-  @HostListener('window:resize') resize() {
-    let width = window.innerWidth;
-    let height = window.innerHeight;
-
-    if( width/height < .8 ){
-      // landscape: width > height
-      this.md_size = true;
-    }
-    else if( width/height > 1.5 ){
-      // vertical: width < height
-      this.md_size = false;
-    } else {
-      // square: width ~= height
-      this.md_size = false;
-    }
   }
 
   setSkills() {
@@ -73,7 +38,7 @@ export class PortfolioComponent implements OnInit {
         degree: 'Master of Science',
         major: 'Software Engineering',
         start_date: 'Aug 2018',
-        end_date: 'current',
+        end_date: 'present',
         gpa: 4.0,
         honors: "",
         coursework: [
@@ -119,36 +84,79 @@ export class PortfolioComponent implements OnInit {
   setExperiences() {
     this.experience = [
       {
+        start_date: 'Sept 2018',
+        end_date: 'present',
+        company: 'META+Lab',
+        position: 'Back-End Developer',
+        projects: [
+          {
+            name: '10Letters',
+            summary: 'A web application for helps transform individuals through goal-setting, self-evaluation/personal accountability, and mentorship.',
+            description: [
+              'Refactored and improved query efficiency with eager loading',
+              'Styled new pages', 'Created middleware',
+              'refactored a function to an API and used Axios to get the data',
+              'Technolgies: Laravel, Vue.js, MySQL, SASS'
+            ],
+
+          }
+        ]
+      },
+      {
+        start_date: 'July 2018',
+        end_date: 'Aug 2018',
+        company: 'META+Lab',
+        position: 'Back-End Intern',
+        projects: [
+          {
+            name: 'ICS Microservice',
+            summary: 'A microservice that creates an ICS file based on a student\'s or professor\'s class schedule.',
+            description: [
+              'Utilized repository pattern to ensure the abstraction of infrastructure and domain',
+              'Leveraged test-driven development to establish a project guideline',
+              'Technologies: Lumen, MySQL, PHPUnit'
+            ],
+
+          }
+        ]
+      },
+      {
         start_date: 'Sept 2016',
         end_date: 'Dec 2017',
         company: 'Learning Resource Center',
         position: 'Mathematics Tutor',
-        description: {
-          intro: 'Provides students with the necessary resources to achieve academic success.',
-          summary: [
-            `Help students understand, study, and apply mathematical concepts; research and
-            upload articles, tutorials, and practice worksheets with solutions onto the CSUN:
-            SMART Lab’s Canvas page to help students better enhance their mathematical skills
-            and knowledge to succeed in their class.`,
-            'Tutored algebra, business calculus, and calculus I.'
-          ]
-        }
+        projects: [
+          {
+            name: '',
+            summary: 'Provides students with the necessary resources to achieve academic success.',
+            description: [
+              `Help students understand, study, and apply mathematical concepts; research and
+              upload articles, tutorials, and practice worksheets with solutions onto the CSUN:
+              SMART Lab’s Canvas page to help students better enhance their mathematical skills
+              and knowledge to succeed in their class.`,
+              'Tutored algebra, business calculus, and calculus I.'
+            ]
+          }
+        ]
       },
       {
         start_date: 'Aug 2015',
         end_date: 'Aug 2016',
         company: 'Center for Academic Success',
         position: 'Mathematics Tutor',
-        description: {
-          intro: 'Empowers students to achieve academic success.',
-          summary: [
-            `Help students understand mathematical concepts and how to succeed in class;
-            attended and participated in tutor trainings to improve tutoring techniques,
-            peer mentoring techniques, learning strategies, lesson planning, working with
-            groups, public speaking, and techniques for working with students with disabilities.`,
-            'Tutored pre-algebra, algebra, trigonometry, business calculus, and calculus I.'
-          ]
-        }
+        projects: [
+          {
+            name: '',
+            summary: 'Empowers students to achieve academic success.',
+            description: [
+              `Help students understand mathematical concepts and how to succeed in class;
+              attended and participated in tutor trainings to improve tutoring techniques,
+              peer mentoring techniques, learning strategies, lesson planning, working with
+              groups, public speaking, and techniques for working with students with disabilities.`,
+              'Tutored pre-algebra, algebra, trigonometry, business calculus, and calculus I.'
+            ]
+          }
+        ]
       }
     ]
   }
